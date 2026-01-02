236655b13713af06edc98d2781fd9aed39a54a44
Jeffchizy <68883740+Jeffchizy@users.noreply.github.com>
2026-01-01 15:22:07 -1100

    Update vite.config.js
    Configure Vite for React 17 classic JSX

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   esbuild: {
-    jsx: "transform",
     jsxFactory: "React.createElement",
     jsxFragment: "React.Fragment"
   }
