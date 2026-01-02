f3b97840ce6bf75e3879ce3d6a51f1ac79ce1a11
Jeffchizy <68883740+Jeffchizy@users.noreply.github.com>
2026-01-01 15:38:09 -1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  esbuild: {
-    jsxFactory: "React.createElement",
-    jsxFragment: "React.Fragment"
-  }
-});
+export default defineConfig({});
